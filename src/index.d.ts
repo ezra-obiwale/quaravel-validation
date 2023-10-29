169a4243ce0d20b368e635ad2d378461face9d7c
Ezra Obiwale <ezra.obiwale@gmail.com>
2023-10-29 20:44:33 +0100

    Add TS typing file

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+declare module '@ezraobiwale/quaravel-validation' {
+  export function getRules(
+    rules: string | string[],
+    messages?: false | { [field: string]: false | { [rule: string]: string } }
+  ): string;
+}
